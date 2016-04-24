126fef67441194a6f9fce23e4db8eb87dabdbeb9
RJ Goetz <rjgoetz@kristen-millers-macbook-pro.local>
2016-04-24 08:30:19 -0500

    added lightbox

--- week_13/PUBLIC/js/main.js
+++ week_13/PUBLIC/js/main.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
 
   // toggle menu
-
   $(".fa-bars").click(function() {
     $("header nav").toggle();
-  })
+  });
 
-  $(".fa-times").click(function() {
+  $("nav > .fa-times").click(function() {
     $("header nav").toggle();
   });
 
@@ -33,9 +32,9 @@ $(document).ready(function() {
     ;
 
     $(imgBlock).css("height", height);
-  }
+  };
 
-  // // // // //
+  // // // // // // // //
   // grid calculations
   var windowWidth = $(window).width();
 
@@ -47,7 +46,7 @@ $(document).ready(function() {
     var n = 5;
   } else if (windowWidth > 1280) {
     var n = 6;
-  }
+  };
 
   if (n === 3) { // max 768 width
     var gutter = (24 / 768) * windowWidth;
@@ -57,7 +56,7 @@ $(document).ready(function() {
     var gutter = (40 / 1280) * windowWidth;
   } else if (n === 6) { // min 1281 -> max 1600 width
     var gutter = (50 / 1600) * windowWidth;
-  }
+  };
 
 
   // // // // // // // // // // // // // // //
@@ -69,16 +68,13 @@ $(document).ready(function() {
     filter = ""
   ;
 
-  console.log(n);
-  console.log(gutter);
-
   // Create Output
   function createData(insert) {
 
     output += "<div class='" + insert.layout + "'>";
     output += "<img src='" + insert.src + "'>";
     output += "<figure>";
-    output += "<figcaption>" + insert.name + " " + insert.photoDate + "</figcaption>";
+    output += "<figcaption>" + insert.name + "</figcaption>";
     output += "</figure>";
     output += "</div>";
 
@@ -94,19 +90,48 @@ $(document).ready(function() {
 
     for (var i = 0; i < dataToCheck.tags.length; i++) {
       if (dataToCheck.tags[i] === filter) {
-        console.log("MATCH!");
         return true;
 
         break;
       }
-    }
+    };
 
   };
+
+  // check for last in row by looping through photos
+  function checkLastInRow() {
+    if (windowWidth > 414) {
+        for (var i = 1; i < $("section div").length; i++) {
+        pictures = $("section div");
+        prevPicturePos = $(pictures[i-1]).position();
+
+        // get floating point width of previous picture
+        var rect1 = $(pictures)[i-1].getBoundingClientRect();
+        prevPictureWidth = rect1.right - rect1.left;
+
+        // get floating point width of current picture
+        var rect2 = $(pictures)[i].getBoundingClientRect();
+        curPictureWidth = rect2.right - rect2.left;
+        curPicturePos = $(pictures[i]).position();
+
+        // find margin-right value
+        var margin = $(pictures[i-1]).css("margin-right");
+
+        totalWidth = (prevPicturePos.left + prevPictureWidth + gutter + curPictureWidth);
+
+        if (totalWidth > (.99 * windowWidth) && totalWidth <= windowWidth) {
+          $(pictures[i]).css("margin-right", "0");
+        } else if (margin === "0px") {
+          $(pictures[i]).css("clear", "both");
+        }
+      }
+    }
+  }
 
 
   // listen for menu radio button click
 
-  $("body").on("click", "input[name=filter]", function() {
+  $("nav").on("click", "input[name=filter]", function() {
 
     getData();
 
@@ -139,32 +164,7 @@ $(document).ready(function() {
 
       $("section").html(output);
 
-      // check for last in row by looping through photos
-      for (var i = 1; i < $("section div").length; i++) {
-        pictures = $("section div");
-        prevPicturePos = $(pictures[i-1]).position();
-
-        // get floating point width of previous picture
-        rect1 = $(pictures)[i-1].getBoundingClientRect();
-        prevPictureWidth = rect1.right - rect1.left;
-
-        // get floating point width of current picture
-        rect2 = $(pictures)[i].getBoundingClientRect();
-        curPictureWidth = rect2.right - rect2.left;
-        curPicturePos = $(pictures[i]).position();
-
-        totalWidth = (prevPicturePos.left + prevPictureWidth + gutter + curPictureWidth);
-        console.log(totalWidth);
-
-        console.log(i + " " + prevPicturePos.left + " " + prevPictureWidth + " " + curPictureWidth + " " + windowWidth);
-
-        if (totalWidth === windowWidth) {
-          $(pictures[i]).css("margin-right", "0");
-        } else if (curPicturePos.left === 0) {
-          $(pictures[i]).css("clear", "both");
-        }
-
-      }
+      checkLastInRow();
 
 
       if (windowWidth < 415) {
@@ -174,6 +174,140 @@ $(document).ready(function() {
     });
 
   };
+
+
+  // // // // // // // // // // //
+  // Lightbox
+
+  var
+    boxOutput = "",
+    url = "",
+    captionArray = [],
+    capTextArray =[],
+    captionText,
+    figCaption,
+    boxHeight,
+    imgHeight,
+    imgIndex,
+    photosArray,
+    topPosition
+  ;
+
+  function lightbox() {
+
+    boxOutput += "<div class='lightbox'>";
+    boxOutput += "<i class='fa fa-times'></i>"
+    boxOutput += "<img src='" + url + "'>";
+    boxOutput += "<figure>";
+    boxOutput += "<figcaption>" + captionText + "</figcaption>";
+    boxOutput += "</figure>";
+    boxOutput += "<div class='lightbox-nav'>";
+    boxOutput += "<i class='fa fa-chevron-circle-left'></i>";
+    boxOutput += "<i class='fa fa-chevron-circle-right'></i>";
+    boxOutput += "</div>";
+    boxOutput += "</div>";
+
+    $(boxOutput).prependTo("body");
+
+  };
+
+  function boxPosition() {
+
+    imgHeight = $("body > div img").height();
+    boxHeight = $(window).height();
+    topPosition = ((boxHeight - imgHeight) / 2);
+
+    $("body > div").css("padding-top", topPosition);
+
+  };
+
+  function arrowPosition() {
+    arrowHeight = 96;
+    boxHeight = $(window).height();
+    topPosition = ((boxHeight - arrowHeight) / 2);
+
+    $("body .lightbox-nav").css("padding-top", topPosition);
+  }
+
+  function imgLocation() {
+
+    var urlArray = [];
+
+    for (var i = 0; i < photosArray.length; i++) {
+      var tempURL = $(photosArray[i]).attr("src");
+      urlArray.push(tempURL);
+    }
+
+    imgIndex = $.inArray(url, urlArray);
+
+  };
+
+
+  // initiate lightbox after ajax loads
+  $(document).ajaxComplete(function() {
+    $("div").on("click", "img", function() {
+
+      url = $(this).attr("src");
+      figCaption = $(this).next();
+      captionText = figCaption.text();
+      photosArray = $("section div img");
+      captionArray = $("section div figcaption");
+
+      imgLocation();
+      lightbox();
+      boxPosition();
+      if (windowWidth > 599) {
+        arrowPosition();
+      }
+
+    })
+
+  });
+
+  // remove picture from DOM by clicking close icon
+
+  $("body").on("click", ".lightbox .fa-times", function() {
+    $(".lightbox").remove();
+    boxOutput = "";
+  });
+
+  // navigate to next photo
+
+  $("body").on("click", ".lightbox .fa-chevron-circle-right", function() {
+    $(".lightbox").remove();
+    boxOutput = "";
+    if (imgIndex < (photosArray.length - 1)) {
+      url = $(photosArray[imgIndex + 1]).attr("src");
+      captionText = $(captionArray[imgIndex + 1]).text();
+      imgLocation();
+      lightbox();
+      boxPosition();
+      if (windowWidth > 599) {
+        arrowPosition();
+      }
+    }
+
+  });
+
+  // navigate to previous photo
+
+  $("body").on("click", ".lightbox .fa-chevron-circle-left", function() {
+    $(".lightbox").remove();
+    boxOutput = "";
+
+    if (imgIndex > 0) {
+      url = $(photosArray[imgIndex - 1]).attr("src");
+      captionText = $(captionArray[imgIndex - 1]).text();
+      imgLocation();
+      lightbox();
+      boxPosition();
+      if (windowWidth > 599) {
+        arrowPosition();
+      }
+    }
+
+  });
+
 
 
   // initial function calls
